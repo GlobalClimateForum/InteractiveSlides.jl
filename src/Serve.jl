c582d2ced331d844a34c2bede09914eb6e680e51
jochenkrattenmacher <10695828+jochenkrattenmacher@users.noreply.github.com>
2022-08-04 18:38:56 +0200

    serve: import instead of using, also import ModelManager for reset

--- src/Serve.jl
+++ src/Serve.jl
@@ -1,30 +1,30 @@
 module Serve
-using ..Stipple, ..ModelInit, ..UI
+import ..Stipple, ..ModelInit, ..ModelManager, ..UI
 export serve_presentation
 
 function build_presentation(PresModel::DataType, gen_content::Function, gen_auxUI::Function, settings::Dict, request_params::Dict{Symbol, Any})
     hardreset = get(request_params, :hardreset, "0") != "0"
     if hardreset
-        pmodel = get_or_create_pmodel(PresModel; force_create = true)
+        pmodel = ModelInit.get_or_create_pmodel(PresModel; force_create = true)
     else
-        pmodel = get_or_create_pmodel(PresModel)
+        pmodel = ModelInit.get_or_create_pmodel(PresModel)
     end
     println("Time to build UI:")
     if hardreset || get(request_params, :reset, "0") != "0"
         empty!(pmodel.counters)
-        reset_handlers()
+        ModelManager.reset_handlers()
         pop!(Stipple.Layout.THEMES)
     end
-    @time ui(pmodel, gen_content, gen_auxUI, settings, request_params) |> html 
+    @time UI.ui(pmodel, gen_content, gen_auxUI, settings, request_params) |> Stipple.html 
 end
 
 function serve_presentation(PresModel::DataType, gen_content::Function, gen_auxUI::Function, settings::Dict)
-    Genie.route("/") do
-        build_presentation(PresModel, gen_content, gen_auxUI, settings, params())
+    Stipple.route("/") do
+        build_presentation(PresModel, gen_content, gen_auxUI, settings, Stipple.params())
     end
 
-    Genie.route("/:monitor_id::Int/") do
-        build_presentation(PresModel, gen_content, gen_auxUI, settings, params())
+    Stipple.route("/:monitor_id::Int/") do
+        build_presentation(PresModel, gen_content, gen_auxUI, settings, Stipple.params())
     end
 end
 
