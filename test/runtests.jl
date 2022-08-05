a790741e3e3bd9b791eea73bd8727568d7f5e72c
jochenkrattenmacher <10695828+jochenkrattenmacher@users.noreply.github.com>
2022-08-05 14:54:28 +0200

    properly set up CI/continuous integration (hopefully)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,25 @@
 using InteractivePresentations
 using Test
 
-@testset "InteractivePresentations.jl" begin
-    # Write your tests here.
+module TestBasics
+    using ..InteractivePresentations
+    @presentation! struct PresentationModel <: ReactiveModel
+    end
+
+    const settings = Dict{Symbol, Any}(:folder => "test", :num_monitors => 2)
+
+    function gen_auxUI(m_id::Int)
+    end
+
+    function gen_content(pmodel::PresentationModel)
+    end
+
+    serve_presentation(PresentationModel, gen_content, gen_auxUI, settings)
+
+    Genie.up()
+end
+
+@testset "Basics" begin
+    using .TestBasics
+    @show Genie.routes()
 end
