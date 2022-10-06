0dfb2656fd654d96305ab45a5b2656832300b741
jochenkrattenmacher <10695828+jochenkrattenmacher@users.noreply.github.com>
2022-10-06 10:41:55 +0200

    hotkeys don't fire anymore while input active

--- src/js/hotkeys.js
+++ src/js/hotkeys.js
@@ -7,6 +7,9 @@ Vue.directive('hotkeys', {
     iscontroller = !(params.shift == null) || !(params.ctrl == null);
 
     this._keyListener = function(e) {
+      var activeElement = document.activeElement;
+      var inputs = ['input', 'select', 'button', 'textarea'];
+
       team_id = binding.expression
       slide_id = 'PresentationModel.slide_id' + team_id
       slide_state = 'PresentationModel.slide_state' + team_id
@@ -15,6 +18,7 @@ Vue.directive('hotkeys', {
       drawer_controller = 'PresentationModel.drawer_controller' + team_id
       setTimeout("hljs.highlightAll()", 100); //in case highlight.js is being used. Using github.com/highlightjs/vue-plugin/blob/1-stable/src/vue.js presumably would be less dirty
       // the logic below could also be implemented in the form of listeners on slide_state instead (see commit for version 0.18.2), however, the below solution is more performant (immediate switch between slides)
+      if (!inputs.indexOf(activeElement.tagName.toLowerCase()) == -1 || !activeElement.hasAttribute("contenteditable")) { //do nothing if user is writing something
       switch (e.key) {
       case "ArrowRight":
         eval(slide_state + "==" + num_states + "?" + slide_id + '< PresentationModel.num_slides ? (' + slide_id + '++, ' + slide_state + '=1) : null : ' + slide_state + '++;');
@@ -41,7 +45,7 @@ Vue.directive('hotkeys', {
         break;
       };
     }
-
+    }
     document.addEventListener('keydown', this._keyListener.bind(this));
 },
 })
